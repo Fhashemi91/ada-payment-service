0112defacc8f55e9acd50d78269be6a57eab0248
Fhashemi91 <f.hashemi91@gmail.com>
2022-03-16 15:08:17 +0100

    Add returns model.

---
+++ finance_service/models/item_return.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Column, Integer, String, ChoiceType
+from finance_service.database import Base
+from enum import Enum
+
+
+class Status(Enum):
+    new = 1
+    processing = 2
+    rejected = 3
+    accepted = 4
+    refund = 5
+    processed = 6
+
+
+class Return(Base):
+    __tablename__ = "returns"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer)
+    reference_id = Column(Integer)
+    status = Column(Enum(Status))
