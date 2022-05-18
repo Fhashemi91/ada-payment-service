d01f389e7e213356ef8d9f8d80ff51be8c86dfb8
Shahin <me@5hah.in>
2022-05-18 09:41:59 +0200

    add events.

--- payment_service/main.py
+++ payment_service/main.py
@@ -6,6 +6,7 @@ from payment_service.models.user import User
 from payment_service.models.payment import Payment, Status
 from payment_service.database import engine, Base, SessionLocal
 from fastapi.responses import RedirectResponse, HTMLResponse
+from payment_service.pubsub import submit_message
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,11 +25,14 @@ def get_db():
 def register_payment(
     user_id: str, info: schemas.PaymentCreate, db: Session = Depends(get_db)
 ):
+    submit_message("payment register requested", user_id=str(user_id))
     return crud.register_payment(db=db, info=info, user_id=user_id)
 
 
 @app.get("/payments/{user_id}", response_model=list[schemas.Payment])
 def list_payment(user_id: str, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    submit_message("payment list requested", user_id=str(user_id))
+    
     payments = crud.get_payments(db, user_id, skip=skip, limit=limit)
     return payments
 
@@ -56,5 +60,7 @@ def pay(payment_id: int, db: Session = Depends(get_db)):
 
 @app.get("/pay/{payment_id}/success/", response_class=HTMLResponse)
 def update_payments(payment_id: int, db: Session = Depends(get_db)):
+    submit_message("payment update requested", payment_id=str(payment_id))
+    
     payments = crud.update_payment_status(db, payment_id, Status.successful)
     return """<center><h1>Payment was successful</h1></center>"""
